319ea48ffafcf858931146f93c27ab33610ab4eb
nah-ko <>
2003-07-15 20:09:26 +0000

    Now mbot will work with PostgreSQL !

--- mbot/NewsHandler.py
+++ mbot/NewsHandler.py
@@ -12,26 +12,50 @@
 
 import MailHandler
 import sys, os, email
-import MySQLdb, re, Image
+import re, Image
+import MySQLdb, pg
 import ConfigParser
+
+from pg import INV_WRITE
 
 SECTION	= 'NEWS'
 
 class NewsHandler(MailHandler.MailHandler):
 
 	def add_img(self, filename, filetype, filedata, TNfiledata, filesize):
-		db = MySQLdb.connect(db=self.DB, host=self.HOST, user=self.DB_USER, passwd=self.DB_PASS)
+		if self.DB_TYPE == 'mysql':
+			db = MySQLdb.connect(db=self.DB, host=self.HOST, user=self.DB_USER, passwd=self.DB_PASS)
+		elif self.DB_TYPE == 'postgresql':
+			db = pg.connect(dbname=self.DB, host=self.HOST, user=self.DB_USER, passwd=self.DB_PASS)
 		news_id	= self.id
 		TABLE	= "photo_test"
 		desc	= "[News] " + filename
-		myquery = "insert into %s (description,img_data,tnimg_data,filename,filesize,filetype) values ('%s','%s','%s','%s','%d','%s')" % (TABLE, desc, db.escape_string(filedata), db.escape_string(TNfiledata), filename, filesize, filetype)
-		mycur	= db.cursor()
-		mycur.execute(myquery)
-		id	= db.insert_id()
+		if self.DB_TYPE == 'mysql':
+			myquery = "insert into %s (description,img_data,tnimg_data,filename,filesize,filetype) values ('%s','%s','%s','%s','%d','%s')" % (TABLE, desc, db.escape_string(filedata), db.escape_string(TNfiledata), filename, filesize, filetype)
+			mycur	= db.cursor()
+			mycur.execute(myquery)
+			id	= db.insert_id()
+		elif self.DB_TYPE == 'postgresql':
+			db.query("begin")
+			img_LO   = db.locreate(INV_WRITE)
+			TNimg_LO = db.locreate(INV_WRITE)
+			myquery  = "insert into %s (description,img_data,tnimg_data,filename,filesize,filetype) values ('%s','%s','%s','%s','%d','%s')" % (TABLE, desc, img_LO.oid, TNimg_LO.oid, filename, filesize, filetype)
+			img_LO.open(INV_WRITE)
+			img_LO.write(filedata)
+			img_LO.close()
+			TNimg_LO.open(INV_WRITE)
+			TNimg_LO.write(filedata)
+			TNimg_LO.close()
+			db.query("commit")
+			SEQ_TABLE = TABLE + '_id_seq'
+			id	 = db.query("select currval('%s')" % SEQ_TABLE).getresult()[0]
 		TABLE	= "news_test"
-		myquery	= "update %s set id_img='%d' where id='%d'" % (TABLE, id, news_id)
-		mycur   = db.cursor()
-		mycur.execute(myquery)
+		if self.DB_TYPE == 'mysql':
+			myquery	= "update %s set id_img='%d' where id='%d'" % (TABLE, id, news_id)
+			mycur	= db.cursor()
+			mycur.execute(myquery)
+		elif self.DB_TYPE == 'postgresql':
+			req     = db.query("update %s set id_img='%d' where id='%d'" % (TABLE, id, news_id))
 		db.close()
 		return id
 	
@@ -66,13 +90,14 @@ class NewsHandler(MailHandler.MailHandler):
 	def read_conf(self, ConfObj):
 		''' Getting config options for this handler '''
 
-		self.HOST = ConfObj.get(SECTION,'host')
-		self.DB = ConfObj.get(SECTION,'db')
-		self.DB_USER = ConfObj.get(SECTION,'db_user')
-		self.DB_PASS = ConfObj.get(SECTION,'db_pass')
+		self.HOST        = ConfObj.get(SECTION,'host')
+		self.DB          = ConfObj.get(SECTION,'db')
+		self.DB_USER     = ConfObj.get(SECTION,'db_user')
+		self.DB_PASS     = ConfObj.get(SECTION,'db_pass')
+		self.DB_TYPE     = ConfObj.get(SECTION,'db_type')
 		self.ATTACH_PATH = ConfObj.get(SECTION,'attach_path')
-		self.tnX = ConfObj.get(SECTION,'tnx')
-		self.tnY = ConfObj.get(SECTION,'tny')
+		self.tnX         = ConfObj.get(SECTION,'tnx')
+		self.tnY         = ConfObj.get(SECTION,'tny')
 
 	def handle(self, body):
 		"Get news from text and attachment if present"
